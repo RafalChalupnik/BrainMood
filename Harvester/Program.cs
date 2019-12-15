384a4b8bc0c2000e53516fc4deb171f8ae2e8658
Raven <rafal.chalupnik@gmail.com>
2019-12-15 22:16:36 +0100

    WIP: Migrating code found in the Internet

--- Harvester/Program.cs
+++ Harvester/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading;
 
 namespace BrainMood.Harvester
 {
     class Program
     {
-        static void Main(string[] args)
+        async static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new MindWaveClient(new System.Net.Sockets.TcpClient());
+            client.DataReceived += DataReceived;
+
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                var task = client.Run(cancellationTokenSource.Token);
+
+                cancellationTokenSource.Cancel();
+
+                await task;
+            }
+        }
+
+        static void DataReceived(object? sender, MindWaveEventArgs args)
+        {
+
         }
     }
 }
